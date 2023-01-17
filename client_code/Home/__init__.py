c87e0df3534d7c59af5d9650e85fba54524f9996
isabela.santos@discentes.fat.uerj.br <isabela.santos@discentes.fat.uerj.br>
2023-01-17 20:26:54 +0000

    Created Caronas; Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import HomeTemplate
 from anvil import *
+from ..Caronas import Caronas
 
 class Home(HomeTemplate):
   def __init__(self, **properties):
@@ -7,3 +8,10 @@ class Home(HomeTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def botao_carona_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.column_panel.clear()
+    self.column_panel.add_component(Caronas())
+    pass
+
