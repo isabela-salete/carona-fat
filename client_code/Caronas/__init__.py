00aa5d87b5c9e3a6087d372ace5abb884d935ff0
isabela.santos@discentes.fat.uerj.br <isabela.santos@discentes.fat.uerj.br>
2023-02-04 18:56:37 +0000

    Created Bus; Edited Base and Caronas

--- client_code/Caronas/__init__.py
+++ client_code/Caronas/__init__.py
@@ -8,6 +8,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 from ..Criar_carona import Criar_carona
 from ..Carta import Carta
+from ..Bus import Bus
 import anvil.users
 
 class Caronas(CaronasTemplate):
@@ -30,3 +31,10 @@ class Caronas(CaronasTemplate):
      caronas_panel.add_component(c, row=str(i//2), width_xs=6)
      
     self.column_panel_1.add_component(caronas_panel)
+
+  def onibus_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.column_panel_1.clear()
+    self.column_panel_1.add_component(Bus())
+    pass
+
